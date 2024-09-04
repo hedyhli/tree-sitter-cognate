7a9ed6f0bc80886f7f5cd561b16126884582f8cb
~hedy <hedy.dev@protonmail.com>
2024-09-05 07:27:26 +0800

    Fix builtins

--- helix/queries/cognate/highlights.scm
+++ helix/queries/cognate/highlights.scm
@@ -35,7 +35,7 @@
  (#match? @keyword.control "^(With)$"))
 
 ((identifier) @function.builtin
- (#match? @function.builtin "^(Empty|Match|Print|Put|Random|Modulo|Sqrt|Integer\\?|Zero\\?|First|Rest|Head|Tail|Push|Empty\\?|Join|String-length|Substring|Regex|Regex-match|Ordinal|Character|Split|Uppercase|Lowercase|Floor|Round|Ceiling|Abs|Error|Wait|Stop|Show|Stack|Clear|Unbox|Debug|Begin|Sind|Cosd|Tand|Sin|Cos|Tan|Exp|Log|Ln|Asind|Acosd|Atand|Asin|Acos|Atan|Sinhd|Coshd|Tanhd|Sinh|Cosh|Tanh|Table|Insert|Remove|\\.|Has|Values|Keys|Parameters|Input|Open|Read-file|Read-line|Close|Path|Seek|Drop|Prepend|Index|All|Discard|None|Append|Swap|Fold|Prints|Times|Range|Twin|Take|Triplet|Min|Reverse|Filter|Max|When|Case|Map|Puts|Sort|Assert|Length|Of|)"))
+ (#match? @function.builtin "^(Empty|Match|Print|Put|Random|Modulo|Sqrt|Integer\\?|Zero\\?|First|Rest|Head|Tail|Push|Empty\\?|Join|String-length|Substring|Regex|Regex-match|Ordinal|Character|Split|Uppercase|Lowercase|Floor|Round|Ceiling|Abs|Error|Wait|Stop|Show|Stack|Clear|Unbox|Debug|Begin|Sind|Cosd|Tand|Sin|Cos|Tan|Exp|Log|Ln|Asind|Acosd|Atand|Asin|Acos|Atan|Sinhd|Coshd|Tanhd|Sinh|Cosh|Tanh|Table|Insert|Remove|\\.|Has|Values|Keys|Parameters|Input|Open|Read-file|Read-line|Close|Path|Seek|Drop|Prepend|Index|All|Discard|None|Append|Swap|Fold|Prints|Times|Range|Twin|Take|Triplet|Min|Reverse|Filter|Max|Map|Puts|Sort|Assert|Length|Of|)$"))
 
 ((identifier) @operator (#match? @operator "^(\\+|-|\\*|/|>|<|<=|>=|==|!=|\\^)$"))
 
