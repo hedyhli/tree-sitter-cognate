688aedc4189d326f423965c66e53f39673be0f01
~hedy <hedy.dev@protonmail.com>
2024-09-07 10:02:37 +0800

    Update builtins; Support escaping in the script

--- helix/queries/cognate/highlights.scm
+++ helix/queries/cognate/highlights.scm
@@ -35,7 +35,11 @@
  (#match? @keyword.control "^(With)$"))
 
 ((identifier) @function.builtin
- (#match? @function.builtin "^(Empty|Match|Print|Put|Random|Modulo|Sqrt|Integer\\?|Zero\\?|First|Rest|Head|Tail|Push|Empty\\?|Join|String-length|Substring|Regex|Regex-match|Ordinal|Character|Split|Uppercase|Lowercase|Floor|Round|Ceiling|Abs|Error|Wait|Stop|Show|Stack|Clear|Unbox|Debug|Begin|Sind|Cosd|Tand|Sin|Cos|Tan|Exp|Log|Ln|Asind|Acosd|Atand|Asin|Acos|Atan|Sinhd|Coshd|Tanhd|Sinh|Cosh|Tanh|Table|Insert|Remove|\\.|Has|Values|Keys|Parameters|Input|Open|Read-file|Read-line|Close|Path|Seek|Drop|Prepend|Index|All|Discard|None|Append|Swap|Fold|Prints|Times|Range|Twin|Take|Triplet|Min|Reverse|Filter|Max|Map|Puts|Sort|Assert|Length|Of|)$"))
+ (#match? @function.builtin
+ ;;BEGIN
+ "^(Empty|Match|Print|Put|Random|Modulo|Sqrt|Integer\\?|Zero\\?|First|Rest|Push|Empty\\?|Join|Substring|Regex|Regex-match|Ordinal|Character|Split|Uppercase|Lowercase|Floor|Round|Ceiling|Abs|Error|Wait|Stop|Show|Stack|Clear|Unbox|Begin|Sind|Cosd|Tand|Sin|Cos|Tan|Exp|Log|Ln|Asind|Acosd|Atand|Asin|Acos|Atan|Sinhd|Coshd|Tanhd|Sinh|Cosh|Tanh|Table|Insert|Remove|\\.|Has|Values|Keys|Length|Parameters|Input|Open|Read-file|Read-line|Close|Path|Seek|Drop|Prepend|Index|All|Discard|None|Append|Swap|Fold|Prints|Times|Range|Twin|Take|Triplet|Min|Reverse|Filter|Max|Map|Puts|Sort|Assert|Length|Of)$"
+ ;;END
+ ))
 
 ((identifier) @operator (#match? @operator "^(\\+|-|\\*|/|>|<|<=|>=|==|!=|\\^)$"))
 
@@ -43,7 +47,7 @@
  (#match? @keyword.operator "^(Not|And|Or|Xor)$"))
 
 ((identifier) @type.builtin
- (#match? @type.builtin "^(List|Box|Number|Number!|String!|List!|Block!|Boolean!|Symbol!|Box!|Io!|Any!|Number?|String?|List?|Block?|Boolean?|Symbol?|Box?|Io?|Any?|)$"))
+ (#match? @type.builtin "^(List|Box|Number|Number!|String!|List!|Block!|Boolean!|Symbol!|Box!|Io!|Any!|Number\?|String\?|List\?|Block\?|Boolean\?|Symbol\?|Box\?|Io\?|Any\?|)$"))
 
 (statement (
   (identifier) @function.builtin (#eq? @function.builtin ".")
